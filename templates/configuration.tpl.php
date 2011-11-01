0592500c1305f8efa686cd214790e5a0e5617734
Matthias Vandermaesen <matthias@Prometheus.(none)>
2011-11-01 22:37:46 +0100

    Refactored to simplified design

---
+++ templates/configuration.tpl.php
@@ -0,0 +1,26 @@
+<div class="wrap">
+  <h2><?php _e('Mollom Flightdeck', MOLLOM_I18N); ?></h2>
+
+  <div class="narrow">
+    <?php print $messages; ?>
+  <div class="column-left">
+
+    <p><?php _e('Mollom is a web service that helps you identify content quality and, more importantly, helps you stop comment and contact form spam. When moderation becomes easier, you can spend more time and energy to interact with your web community.', MOLLOM_I8N); ?></p>
+	 
+    <form action="options-general.php?page=mollom-key-config" method="post" id="mollom_configuration" style="margin: auto;">
+      <?php settings_fields( 'mollom_configuration_settings' ); ?>
+ 
+      <p><?php _e('You need a public and a private key before you can make use of Mollom. <a href="http://mollom.com/user/register">Register</a> with Mollom to get your keys.', MOLLOM_I18N); ?></p>
+ 
+      <h3><label><?php _e('Public key', MOLLOM_I18N); ?></label></h3>
+      <input type="text" size="35" maxlength="32" name="mollom_public_key" class="mollom-public-key<?php print ' ' . $fault['mollom-public-key']; ?>" value="<?php print $mollom_public_key; ?>" />
+
+      <h3><label><?php _e('Private key', MOLLOM_I18N); ?></label></h3>
+      <input type="text" size="35" maxlength="32" name="mollom_private_key" id="mollom-private-key" value="<?php print $mollom_private_key; ?>" />
+
+			<?php mollom_nonce_field($mollom_nonce); ?>
+      <input type="submit" name="submit" value="<?php _e('Update options &raquo;', MOLLOM_I18N); ?>" id="submit"/>
+    </form>
+  </div> 
+  
+</div>
--- wp-mollom.php
+++ wp-mollom.php
@@ -1,357 +1,155 @@
 <?php
-/* 
-Plugin Name: WP Mollom Core
+
+/*
+Plugin Name: WP Mollom
 Plugin URI: http://wordpress.org/extend/plugins/wp-mollom/
 Description: Enable <a href="http://www.mollom.com">Mollom</a> on your wordpress blog. This plugin provides Mollom core functionality.
 Author: Matthias Vandermaesen
-Version: 0.1.0-alpha
+Version: 2.x-dev
 Author URI: http://www.colada.be
 Email: matthias@colada.be
 */
 
-/*  Copyright 2008, 2009, 2010 Matthias Vandermaesen  (email : matthias@colada.be) 
-   This program is free software; you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation; either version 2 of the License, or 
-   (at your option) any later version.
-   This program is distributed in the hope that it will be useful, 
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
-   You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software
-   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+/* Copyright 2008, 2009, 2010, 2011 Matthias Vandermaesen (email : matthias@colada.be)
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 */
 
-/* define version information */
-define( 'MOLLOM_CORE_VERSION', '0.1.0-alpha' );
-define( 'MOLLOM_USER_AGENT', 'WP Mollom for Wordpress ' . MOLLOM_CORE_VERSION );
-
-/* define WP Mollom's Watchdog table */
-define( 'MOLLOM_WATCHDOG', 'mollom_watchdog');
-
-/* define WP Mollom's comment table */
-define( 'MOLLOM_TABLE', 'mollom' );
+/* define this version of the plugin */
+define( 'MOLLOM_PLUGIN_VERSION', '2.x-dev' );
 
 /* define WP Mollom's i18n namespace */
 define( 'MOLLOM_I18N', 'wp-mollom' );
 
-/* define the path to the plugin directory */
+/* define a few paths */
 define( 'MOLLOM_PLUGIN_PATH', 'wp-content/plugins/' . dirname(plugin_basename(__FILE__)) );
 define( 'MOLLOM_BASE_PATH', ABSPATH . MOLLOM_PLUGIN_PATH );
 
-/* define which messages are kept in the watchdog */
-define ( 'MOLLOM_LOG_LEVEL', 1 );
+class WPMollom {
 
-class WP_Mollom {
+  // Static objects as singletons
+  static private $instance = NULL;
+	static private $mollom = NULL;
+	private $mollom_nonce = 'mollom-configuration';
 	
-	private $mollom;
-	
-  function __construct() {
-	  require_once(ABSPATH . '/wp-content/plugins/' . dirname(plugin_basename(__FILE__)) . '/includes/common.inc.php');
-    require_once(ABSPATH . '/wp-content/plugins/' . dirname(plugin_basename(__FILE__)) . '/includes/templates.inc.php');
-		
-    // load an instance of type Mollom
-    $this->mollom = WP_Mollom::factory();
-    $this->refresh_statistics();
-
-  	// load the text domain for localization
-	  load_plugin_textdomain(MOLLOM_I18N, false, dirname(plugin_basename(__FILE__)));
-	
-	  // register the installer
-   	register_activation_hook(__FILE__, array(&$this, 'installer'));
-
+	/**
+	 * Constructor
+	 */
+	private function __construct() {
+		// load the text domain for localization
+    load_plugin_textdomain(MOLLOM_I18N, false, dirname(plugin_basename(__FILE__)));
     // register the administration page
-	  add_action('admin_menu',array(&$this, 'register_administration_page'));
-	
-	  // register a poormans' cron function
-	  add_action('init', array(&$this, 'cron'));
-	
-	  // register check_comment function
-	  add_action('preprocess_comment', array(&$this, 'check_comment'));
-	
-  	//add_action('wp_set_comment_status', array(&$this, 'mollom_manage_wp_queue'));
-	 /* add_action('preprocess_comment', array(&$this, 'check_comment')); 
-		add_action("admin_print_scripts", array(&$this, 'js_libs'));
-    add_action("admin_print_styles", array(&$this, 'style_libs'));
-    add_action('wp_ajax_mollom_statistics', array(&$this, 'statistics'));	 */
+    add_action('admin_menu',array(&$this, 'register_administration_pages'));
   }
 
-  function js_libs() {
-	  wp_enqueue_script('jquery');
-	  wp_enqueue_script('thickbox');
+	/**
+   * getInstance
+   * Instantiates WPMollom
+   */
+  public function getInstance() {
+    if (!self::$instance) {
+      self::$instance = new WPMollom();
+      return self::$instance;
+	  }
   }
-
-  function style_libs() {
-	  wp_enqueue_style('thickbox');
-  }
-
-  function statistics() {
-	  // @todo remove someDomain string
-	  ?>
-	  <embed src="http://mollom.com/statistics.swf?key=<?php echo get_option('mollom_public_key'); ?>"
-		quality="high" width="600" height="425" name="Mollom" align="middle"
-		play="true" loop="false" allowScriptAccess="sameDomain"
-		type="application/x-shockwave-flash"
-		pluginspage="http://www.adobe.com/go/getflashplayer"></embed>
-	  <?php
-	  exit();
-  }
-
-  // @todo: revise this
-  function refresh_statistics() {
-	  // refresh statistics every hour. We don't want to make an API call with each request
-    $_statistics = get_option('mollom_statistics');
-    if ($_statistics['cache_time'] < (time() + 3600)) {
-	    $statistics = array();
-			$statistic_types = array('total_days',
-									 'total_accepted',
-									 'total_rejected',
-									 'yesterday_accepted',
-									 'yesterday_rejected',
-									 'today_accepted',
-									 'today_rejected',
-		  );
 	
-  	  foreach ($statistic_types as $type) {
-      	$statistic = $this->mollom->getStatistics($type);
-        if ($statistic === FALSE) {
-	        return FALSE;
-        }
-        $statistics['mollom_' . $type] = $statistic;	
-	
-	      // @todo watchdog if error!
-	      $statistics['cache_time'] = time();
-      }
-      update_option('mollom_statistics', serialize($statistics));
-    }	
-  }
-
-  static function factory() {
-    require_once(ABSPATH . '/wp-content/plugins/' . dirname(plugin_basename(__FILE__)) . '/includes/classes/wpmollom.class.php');
+	public function getMollomInstance() {
+		self::mollom_include('mollom.class.inc');
+		self::mollom_include('mollom.wordpress.inc');
 		
-		$mollom =& new WPMollom(get_option('mollom_public_key'), get_option('mollom_private_key'));
+		if (!self::$mollom) {
+			self::$mollom = new MollomWordpress();
+			return self::$mollom;
+		}
 		
-		$mollom_servers = get_option('mollom_servers', NULL);
-		$mollom->setServerList($mollom_servers);
-						
-		return $mollom;
-  }
-
-  static function register_administration_tabs() {
-	  return array(
-	    'core_configuration' => array(
-		     'name'  => 'Configuration',
-		     'class' => 'Configuration_Page',
-		     'file'  => 'core.configuration.class.php',
-	     ),
-	    'core_watchdog' => array(
-		     'name' => 'Watchdog',
-		     'class' => 'Watchdog_Page',
-		     'file'  => 'core.watchdog.class.php',
-		  ),
-		  'core_text_blacklist' => array(
-			   'name' => 'Text blacklist',
-			   'class' => 'Text_Blacklist_Page',
-			   'file' => 'core.textblacklist.class.php',
-			),
-      'client_configuration' => array(
- 	      'name' => 'Comment configuration',
-	      'class' => 'Client_Configuration_Page',
-	      'file' => 'client.configuration.class.php',
-	    ),
-	  );
+	}
+	
+	/**
+	 * Include files, take care of paths
+	 * @param type $file 
+	 */
+	static private function mollom_include($file) {
+		require_once(MOLLOM_BASE_PATH . '/includes/' . $file);
 	}
 	
   /**
    * Register pages in the configuration section
    */
-  function register_administration_page() {
-	  add_submenu_page('options-general.php', __('Mollom', MOLLOM_I18N), __('Mollom', MOLLOM_I18N), 'manage_options', 'mollom-key-config', array(&$this, 'configuration_page'));
-	  add_action('admin_init', array(&$this, 'register_configuration_options'));
+  public function register_administration_pages() {
+    add_submenu_page('options-general.php', __('Mollom', MOLLOM_I18N), __('Mollom', MOLLOM_I18N), 'manage_options', 'mollom-key-config', array(&$this, 'configuration_page'));
+    add_submenu_page('edit-comments.php', __('Mollom', MOLLOM_I18N), __('Mollom', MOLLOM_I18N), 'manage_options', 'mollom-key-config', array(&$this, 'moderation_page'));
+    add_action('admin_init', array(&$this, 'register_configuration_options'));
   }
-
-  function register_configuration_options() {
-    register_setting('mollom_configuration_settings', 'mollom_public_key');
-    register_setting('mollom_configuration_settings', 'mollom_private_key');
-    register_setting('mollom_configuration_settings', 'mollom_roles');
-    register_setting('mollom_configuration_settings', 'mollom_site_policy'); 
-    register_setting('mollom_configuration_settings', 'mollom_reverse_proxy');
-    register_setting('mollom_configuration_settings', 'mollom_reverse_proxy_addresses');
+	
+	/**
+	 * Register settings with Wordpress
+	 */
+	public function register_configuration_options() {
+    register_setting('mollom_settings', 'mollom_public_key');
+    register_setting('mollom_settings', 'mollom_private_key');
+    register_setting('mollom_settings', 'mollom_roles');
+    register_setting('mollom_settings', 'mollom_site_policy');
+    register_setting('mollom_settings', 'mollom_reverse_proxy');
+    register_setting('mollom_settings', 'mollom_reverse_proxy_addresses');
   }
-
-  function configuration_page() {
-	  // fetch tabs and set a default page
-		$active_tab = (!isset($_GET['tab'])) ? 'core_configuration' : $_GET['tab'];
-		mollom_load_page($active_tab);
-	}	
-
-  /**
-   * Installer
-   *
-   * Installs WP Mollom. Creates database tables and variables in the {prefix}_options table
-   */
-  function installer() {
-		if(!get_option('mollom_ham_count'))
-			add_option('mollom_ham_count', 0);
-		if(!get_option('mollom_spam_count'))
-			add_option('mollom_spam_count', 0);
-		if(!get_option('mollom_unsure_count'))
-			add_option('mollom_unsure_count', 0);
-		if(!get_option('mollom_count_moderated'))
-			add_option('mollom_count_moderated', 0);
-		if(!get_option('mollom_site_policy'))
-			add_option('mollom_site_policy', true);
-		if(!get_option('mollom_statistics'))
-			add_option('mollom_statistics', NULL);
-		if(!get_option('mollom_roles')) {
-			$mollom_roles = array();
-			foreach ($wp_roles->roles as $role => $data) {		
-				$mollom_roles[] = $role;
-			}
-			add_option('mollom_roles', serialize($mollom_roles));
-		}	
 	
-	  $sql = "watchdog_ID BIGINT( 20 ) UNSIGNED NOT NULL auto_increment,
-            comment_ID BIGINT( 20 ) NULL DEFAULT NULL,
-	          type VARCHAR ( 40 ) NULL DEFAULT NULL,
-	          message LONGTEXT NULL,
-  	        severity SMALLINT ( 1 ) NOT NULL DEFAULT NULL,
-            created datetime NOT NULL default '0000-00-00 00:00:00',
-	         PRIMARY KEY ( watchdog_ID )";
-
-	  mollom_run_install(MOLLOM_WATCHDOG, '1', $sql);
+	/**
+	 * Handle the configuration page
+	 */
+	public function configuration_page() {
+		self::mollom_include('common.inc');
 	
-		if (get_option('mollom_version_api') != MOLLOM_VERSION) {
-			// updates of the database if the plugin  was already installed
-			$version = MOLLOM_VERSION;
-			update_option('mollom_version_api', $version);
-		}
+		$mollom_public_key = NULL;
+		$mollom_private_key = NULL;
+		$mollom = self::getMollomInstance();
+		$mollom = new MollomWordpress();
+		$m = $mollom->verifyKeys();
+		var_dump($m);
 		
-  	mollom_watchdog('installer', 'Mollom successfully installed');
-  }
-
-	/** 
-	* mollom_check_comment
-	* Check if a comment is spam or ham
-	* @param array $comment the comment passed by the preprocess_comment hook
-	* @return array The comment passed by the preprocess_comment hook
-	*/
-	function check_comment($comment) {
-		$mollom_comment_data = array('post_body' => $comment['comment_content'],
-									 'author_name' => $comment['comment_author'],
-									 'author_url' => $comment['comment_author_url'],
-									 'author_mail' => $comment['comment_author_email']);
-
-		$result = $this->mollom->checkContent($mollom_comment_data); //('mollom.checkContent', $mollom_comment_data);	
-		return $comment;
+	  if ( isset($_POST['submit']) ) {
+  		if ( function_exists('current_user_can') && !current_user_can('manage_options') ) {
+				die(__('Cheatin&#8217; uh?'));
+			}
+			
+			check_admin_referer( $this->mollom_nonce );
+			
+			if ( $_POST['mollom_public_key'] ) {
+				$mollom_public_key = preg_replace( '/[^a-z0-9]/i', '', $_POST['mollom_public_key'] );
+				update_option('mollom_public_key', $mollom_public_key);
+			}
+			
+			if ( $_POST['mollom_private_key'] ) {
+				$mollom_private_key = preg_replace( '/[^a-z0-9]/i', '', $_POST['mollom_private_key'] );
+				update_option('mollom_private_key', $mollom_private_key);
+			}
+    }
+		
+		$vars['mollom_nonce'] = $this->mollom_nonce;
+		$vars['mollom_public_key'] = ($mollom_public_key) ? $mollom_public_key : get_option('mollom_public_key');
+    $vars['mollom_private_key'] = ($mollom_private_key) ? $mollom_private_key : get_option('mollom_private_key');
+		
+	  mollom_theme('configuration', $vars);
 	}
 	
-	
-	function stub_check_comment($comment) {
-		global $mollom_sessionid;
-
-		// if it's a trackback, pass it on
-		if($comment['comment_type'] == 'trackback') {
-			mollom_check_trackback($comment);
-		}
-
-		$private_key = get_option('mollom_private_key');
-		$public_key = get_option('mollom_public_key');
-
-		// check if the client is configured all toghether
-		if ((empty($private_key)) || (empty($public_key))) {
-			if (get_option('mollom_site_policy')) {
-				wp_die(__('You haven\'t configured Mollom yet! Per the website\'s policy. We could not process your comment.', MOLLOM_I8N));
-			}
-		}
-
-		// only check the captcha if there is an active Mollom session
-		// skip to the captcha check if session ID was $_POST'ed
-		
-		// @todo: change this into a nonce check. The CAPTCHA form should get a nonce upon
-		// generation. If that nonce exists and validates: execute the CAPTCHA logic. If the
-		// nonce doesn't exist: treat it as a regular comment. If the nonce fails, display
-		// an error
-		
-		if ($_POST['mollom_sessionid']) {
-			$comment = mollom_check_captcha($comment);
-			return $comment;
-		} else {		
-			// If a logged in user exists check if the role is exempt from a Mollom check
-			// non-registered visitors don't have a role so their submissions are always checked
-			$user = wp_get_current_user();
-			if ($user->ID) {
-				$mollom_roles = unserialize(get_option('mollom_roles'));
-				$detected = array_intersect($user->roles, $mollom_roles);
-				if (count($detected) > 0) {			
-					return $comment;
-				}
-			}		
-
-			$mollom_comment_data = array('post_body' => $comment['comment_content'],
-										 'author_name' => $comment['comment_author'],
-										 'author_url' => $comment['comment_author_url'],
-										 'author_mail' => $comment['comment_author_email'],
-										 'author_ip' => _mollom_author_ip());
-
-			$result = mollom('mollom.checkContent', $mollom_comment_data);
-
-			// quit if an error was thrown else return to WP Comment flow
-			if (function_exists('is_wp_error') && is_wp_error($result)) {
-				if(get_option('mollom_site_policy')) {
-					wp_die($result, __('Something went wrong...', MOLLOM_I8N));
-				} else {
-					return $comment;
-				}
-			}
-
-			$mollom_sessionid = $result['session_id'];
-
-			switch($result['spam']) {
-				case MOLLOM_ANALYSIS_HAM: {
-					// let the comment pass
-					global $spaminess;
-					$spaminess = $result['quality'];
-					_mollom_set_plugincount("ham");
-					add_action('comment_post', '_mollom_save_session', 1);
-					return $comment;
-				}
-				case MOLLOM_ANALYSIS_UNSURE: {
-					// show a CAPTCHA and and set the count of blocked messages
-					_mollom_set_plugincount("spam");
-					$mollom_comment = _mollom_set_fields($_POST, $comment);
-					$mollom_comment['mollom_sessionid'] = $result['session_id'];
-					$mollom_comment['mollom_spaminess'] = $result['quality'];
-					mollom_show_captcha('', $mollom_comment);
-					die();
-				}
-				case MOLLOM_ANALYSIS_SPAM: {
-					// kill the process here because of spam detection and set the count of blocked messages
-					_mollom_set_plugincount("spam");	
-					wp_die(__('Your comment has been marked as spam or unwanted by Mollom. It could not be accepted.', MOLLOM_I8N));
-				}
-				default: {
-					// default behaviour: trigger an error (policy mode)
-					if (function_exists('is_wp_error') && is_wp_error($result)) {
-						if(get_option('mollom_site_policy')) {
-							wp_die($result, __('Something went wrong...', MOLLOM_I8N));
-						} else {
-							return $comment;
-						}
-					}
-				}
-			}
-		}
-
-		// last resort: die to protect the db	
-		wp_die(-6, __('Something went wrong...', MOLLOM_I8N));
+	/**
+	 * Handle the moderation page
+	 */
+	public function moderation_page() {
+		$wp_list_table = _get_list_table('WP_Comments_List_Table');
+    $pagenum = $wp_list_table->get_pagenum();
+		mollom_theme('moderation');
 	}
-	
-	function cron() {
-		purge_mollom_watchdog();
-	}
+
 }
 
-// Work it, baby!
-$mollom = new WP_Mollom();
+// Gone with the wind
+WPMollom::getInstance();
