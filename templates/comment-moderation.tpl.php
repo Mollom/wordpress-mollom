baa8cfd60fb4a04e3a52a13671dacefb44019a7f
Matthias Vandermaesen <matthias@colada.be>
2013-01-03 13:54:59 +0100

    Improved: Mollom info in the moderation queue.

--- templates/comment-moderation.tpl.php
+++ templates/comment-moderation.tpl.php
@@ -1,1 +1,13 @@
-<?php print $spam_classification; ?>
+<?php if ($spam_classification == 'ham') : ?>
+
+<?php _e('Ham', I18N_MOLLOM); ?> 
+
+<?php elseif ($spam_classification == 'unsure') : ?>
+
+<?php _e('Unsure', I18N_MOLLOM); ?>
+
+<?php else : ?>
+
+<?php _e('No information', I18N_MOLLOM); ?> 
+
+<?php endif; ?>
