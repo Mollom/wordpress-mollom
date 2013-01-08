e9d9df80478e136cb2389e7be03a558a4603e754
Matthias Vandermaesen <matthias@colada.be>
2013-01-08 21:48:34 +0100

    Added: a stylesheet for edit-comments.php + css to reduce the mollom column width

--- wp-mollom.php
+++ wp-mollom.php
@@ -35,10 +35,16 @@ define('MOLLOM_PLUGIN_VERSION', '2.x-dev');
  */
 define('MOLLOM_I18N', 'wp-mollom');
 
-/** define a few paths
- *
+/** 
+ * define the plugin path
  */
 define('MOLLOM_PLUGIN_PATH', plugin_dir_path(__FILE__));
+
+/**
+ * define the plugin url
+ */
+define('MOLLOM_PLUGIN_URL', plugin_dir_url(__FILE__));
+
 
 /**
  * define WP Mollom table where mollom data per comment gets stored
