cc51012496e2540ea35f23ba0b4cb2620ac6e987
sun <sun@unleashedmind.com>
2013-08-04 15:08:53 +0200

    Fixed faulty link URLs in fallback mode description.

--- includes/Admin.php
+++ includes/Admin.php
@@ -106,8 +106,8 @@ class MollomAdmin {
       ),
       'value' => get_option('mollom_fallback_mode', 'accept'),
       'description' => vsprintf(__('In case Mollom services are unreachable, no text analysis can be performed and no CAPTCHAs can be generated. Customers on <a href="%s">paid plans</a> have access to <a href="%s">Mollom\'s high-availability backend infrastructure</a>, not available to free users, reducing potential downtime.', MOLLOM_L10N), array(
-        'https://mollom.com/web-service-privacy-policy',
-        'https://mollom.com/terms-of-service',
+        'https://mollom.com/pricing',
+        'https://mollom.com/standard-service-level-agreement',
       )),
     ));
     add_settings_field('mollom_privacy_link', __('Privacy policy link', MOLLOM_L10N), array('MollomForm', 'printItemArray'), 'mollom', 'mollom_options', array(
@@ -116,8 +116,8 @@ class MollomAdmin {
       'label' => __("Link to Mollom's privacy policy", MOLLOM_L10N),
       'value' => get_option('mollom_privacy_link'),
       'description' => vsprintf(__('Displays a link to the recommended <a href="%s">privacy policy on mollom.com</a> on all protected forms. When disabling this option, you are required to inform visitors about data privacy through other means, as stated in the <a href="%s">terms of service</a>.', MOLLOM_L10N), array(
-        '@privacy-policy-url' => 'https://mollom.com/web-service-privacy-policy',
-        '@terms-of-service-url' => 'https://mollom.com/terms-of-service',
+        'https://mollom.com/web-service-privacy-policy',
+        'https://mollom.com/terms-of-service',
       )),
     ));
 
@@ -134,7 +134,6 @@ class MollomAdmin {
       'name' => 'mollom_testing_mode',
       'label' => __('Enable Mollom testing mode', MOLLOM_L10N),
       'value' => get_option('mollom_testing_mode'),
-      // @todo Sanitize.
       'description' => __('Submitting "ham", "unsure", or "spam" on a protected form will trigger the corresponding behavior. Image CAPTCHAs will only respond to "correct" and audio CAPTCHAs only respond to "demo". This option should be disabled in production environments.', MOLLOM_L10N),
     ));
   }
